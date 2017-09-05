2cc1e63b4ca90775ce0bfbdbb7ac7b288e64d804
Aditya <aditya.rao.in@gmail.com>
2017-09-05 21:55:07 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var fs = require('fs');
+var port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, function () {
+    console.log('\x1b[36mjson-server is running!');
+});
